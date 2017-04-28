51a25f820151b3b84eb84641c7c0dd8f22fecf30
Nick Heiner <nth23@cornell.edu>
2017-04-28 13:21:49 -0400

    Firing an action when an unauth user attempts to create a conversation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,44 @@
 import React, {PureComponent} from 'react';
 import ConversationList from './ConversationList';
 import {Link} from 'react-router';
+import {firebaseConnect} from 'react-redux-firebase';
+import {connect} from 'react-redux';
 import {Button} from 'semantic-ui-react';
 
+@firebaseConnect()
+@connect(
+  ({firebase}) => ({
+    isInitializing: firebase.get('isInitializing'),
+    currentUser: firebase.get('auth')
+  }),
+  dispatch => ({
+    onUnauthenticatedUserAttemptToCreateConversation() {
+      dispatch({
+        type: 'UNAUTH_USER_ATTEMPT_CREATE_CONVERSATION'
+      });
+    }
+  })
+)
 class HomePage extends PureComponent {
   render() {
+    const getAddButton = () => {
+      if (this.props.isInitializing) {
+        return null;
+      }
+
+      if (this.props.currentUser) {
+        return <Link to="/conversation/new"><PlusButton /></Link>;
+      }
+
+      return <PlusButton onClick={this.props.onUnauthenticatedUserAttemptToCreateConversation} />;
+    }
     return <div>
         <ConversationList />
-        <Link to="/conversation/new" className="new-button"><Button icon="plus" /></Link>
+        <div className="new-button">{getAddButton()}</div>
     </div>;
   }
 }
+
+const PlusButton = props => <Button icon="plus" {...props} />
 
 export default HomePage;
